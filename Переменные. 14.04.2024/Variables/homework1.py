52fa71402d6df747a8e5f96479c63ec91fbc82aa
So1arDuck <166930616+So1arDuck@users.noreply.github.com>
2024-04-14 19:32:19 +0300

    Add files via upload

---
+++ Переменные. 14.04.2024/Variables/homework1.py
@@ -0,0 +1,12 @@
+name = ("num_1")
+num_1 = 5
+print(int(num_1))
+name = ("num_2")
+num_2 = 3
+print(int(num_2))
+name = ("sum")
+sum = (int(num_1 + num_2))
+print(sum)
+name = ("result")
+result = (int(sum + num_1 * num_2))
+print(result)
