d5774228e15aa1111f3ce09964b7863654cb6090
So1arDuck <166930616+So1arDuck@users.noreply.github.com>
2024-04-16 20:15:29 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+my_string = input("Вы знакомы с законами квантовой механики? ") #Нет, но я знаю что Крокозябры живут на Луне
+print(len(my_string))
+print(my_string .lower())
+print(my_string .upper())
+print(my_string .replace(" ", ""))
+print(my_string[0:1])
+print(my_string[-1])
+
+#tnx_for_exp = itsRealyCool
