0d442fe3ad5f225f0a5787b3a676008bfed803bd
So1arDuck <166930616+So1arDuck@users.noreply.github.com>
2024-04-23 19:04:04 +0300

    Add files via upload

---
+++ homework7.py
@@ -0,0 +1,40 @@
+x = 13
+
+print("Саламчик!")
+if x < 0:
+    print("Меньше нуля")
+    print("Покасики")
+print("End line")
+
+a = 9
+b = 4
+if a > b:
+ print("a, b")
+
+if a > b and b> 0:
+ print("Успех")
+
+if a > b and (a > 0 or b < 1000):
+ print("Успех")
+
+if a < 5 and b < 10:
+ print('Успех')
+print('End line')
+
+if '34' > '123':
+    print('Успех')
+
+    if '123' > '3':
+        print('Успех')
+
+        if [1, 2] > [1, 1]:
+            print('Успех')
+print('End line')
+
+if  '6' > 5:
+    print('Успех')
+    if [5, 6] > 5:
+        print('Успех')
+        if '6' != 5:
+            print('Успех')
+            print("End line")
