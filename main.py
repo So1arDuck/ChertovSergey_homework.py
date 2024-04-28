d1f4095cd2e41960c1a82c977224bfeb9467991b
So1arDuck <166930616+So1arDuck@users.noreply.github.com>
2024-04-28 19:13:06 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+cars_ = ["BMW", "MB", "LADA", "KIA", "HONDA"]
+for i in cars_:
+    for j in [10]:
+        print (f'Я езжу на автомобиле марки {i} + {j}')
+
+
+
+# while 1 > 0:
+#     print('Hello. world')
+
+# list_ = ['while', 'in', 'treat']
+# for i in list_:
+#     if i == 'in':
+#         list_.remove(i)
+#
+#     print(list_)
+
+# list_ = ['one', 'two', 'three']
+# list_2 = [4, 3, 2, 5, 1]
+# sum_ = 0
+#
+# for i in range(len(list_2)):
+#         sum_ += list_2[i]
+#
+# print(sum_) # Если поставить это единой частью к циклу, то он будет выполнять все последовательно, а не посчитает конечные результат сразу.
+
+# for i in range(1, 11):
+#     for j in range(1, 11):
+#         print (f'{i} x {j} = {i * j}')
+#
+# """"
+# 1x1
+# 1x2
+# 1x3
+# """""
